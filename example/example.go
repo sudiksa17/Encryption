efdedeeaf6626202c79b78c1dedb297ec32f9042
sudikshasrivastava <sudiksha1701@gmail.com>
2020-05-27 14:57:50 +0530

    example added

--- example/example.go
+++ example/example.go
@@ -1,8 +1,7 @@
 package example
 
 import (
-	"Encryption/algos/aes"
-	"Encryption/algos/rsa"
+	"github.com/sudiksa17/Encryption/encryption"
 )
 
 func example() {
@@ -10,18 +9,18 @@ func example() {
 	text := "text"
 	//For AES and AES with base64 encoded
 	//pass key and text that you want to encrypt
-	encryptedData := aes.Encrypt(key, text)
+	encryptedData := encryption.AESEncrypt(key, text)
 	//then for decrypting the data, pass the string
 	//that is returned from the above function, it is the encrypted data and pass key along wiht it
-	aes.Decrypt(key, encryptedData)
+	encryption.AESDecrypt(key, encryptedData)
 
 	//Same for AES with base64 encoded
-	encryptedData64 := aes.Encrypt64(key, text)
-	aes.Decrypt64(key, encryptedData64)
+	encryptedData64 := encryption.AES64Encrypt(key, text)
+	encryption.AES64Decrypt(key, encryptedData64)
 
 	//For RSA Algorithm you just need to pass the text string, Encrypt function
 	//will return encryptedData as well as a private key that will be used for decryption
-	rsaEncryptedData, privateKey := rsa.Encrypt(text)
-	rsa.Decrypt(rsaEncryptedData, privateKey)
+	rsaEncryptedData, privateKey := encryption.RSAEncrypt(text)
+	encryption.RSADecrypt(rsaEncryptedData, privateKey)
 
 }
